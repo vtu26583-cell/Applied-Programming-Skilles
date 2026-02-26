9cdac65f798865544bb28483251581668379fd04
vtu26583-cell <vtu26583@veltech.edu.in>
2026-02-26 11:57:24 +0530

    Time: 3 ms (87.50%) | Memory: 43.2 MB (73.94%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        if (s.length() % 2 != 0) return false;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } 
+
+            else {
+
+                if (stack.isEmpty()) return false;
+                
+                char top = stack.pop();
+
+                if (c == ')' && top != '(') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ']' && top != '[') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
