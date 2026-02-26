aa3bd309128a3b379f1146d078c7739ec7690d63
vtu26583-cell <vtu26583@veltech.edu.in>
2026-02-26 09:57:25 +0530

    Time: 1 ms (39.99%) | Memory: 46.9 MB (31.53%) - LeetSync

---
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -0,0 +1,26 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        java.util.Stack<TreeNode> stack = new java.util.Stack<>();
+        
+        while (true) {
+            // Traverse left subtree
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            
+            root = stack.pop();
+            k--;
+            if (k == 0) return root.val;
+            
+            root = root.right;
+        }
+    }
+}
