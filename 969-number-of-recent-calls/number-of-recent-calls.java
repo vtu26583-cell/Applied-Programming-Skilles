5d00ab21c693281da82544cda150b17d8d9dde75
vtu26583-cell <vtu26583@veltech.edu.in>
2026-02-26 12:08:40 +0530

    Time: 23 ms (31.55%) | Memory: 59.3 MB (68.95%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class RecentCounter {
+    Queue<Integer> q;
+
+    public RecentCounter() {
+        q = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        q.offer(t);
+        while (q.peek() < t - 3000) {
+            q.poll();
+        }
+        return q.size();
+    }
+}
