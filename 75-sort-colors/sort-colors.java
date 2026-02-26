7237b1696ddff5a8e61ba66a895105e69a141cf5
vtu26583-cell <vtu26583@veltech.edu.in>
2026-02-26 11:50:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (60.99%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,16 @@
+class Solution{
+public void sortColors(int[] nums){
+int l=0,m=0,r=nums.length-1;
+while(m<=r){
+if(nums[m]==0){
+int t=nums[l];nums[l]=nums[m];nums[m]=t;
+l++;m++;
+}else if(nums[m]==1){
+m++;
+}else{
+int t=nums[m];nums[m]=nums[r];nums[r]=t;
+r--;
+}
+}
+}
+}
