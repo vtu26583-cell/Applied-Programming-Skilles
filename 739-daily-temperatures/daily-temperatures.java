5ad3da80bff862198033544a3116c31e8f37ac2d
vtu26583-cell <vtu26583@veltech.edu.in>
2026-02-26 11:56:09 +0530

    Time: 60 ms (77.43%) | Memory: 107.8 MB (25.07%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) { 
+            
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int previousDayIndex = stack.pop();
+                ans[previousDayIndex] = i - previousDayIndex;
+            }
+            stack.push(i);
+            
+        } 
+
+        return ans;
+    }
+}
