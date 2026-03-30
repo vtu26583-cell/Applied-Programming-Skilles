dbe20e78466a0f1ec8f24ad8a419d1a60b0de893
UTUKURU SAI SANATH <vtu26583@veltech.edu.in>
2026-03-30 09:52:55 +0530

    Time: 1 ms (85.88%) | Memory: 46.3 MB (57.84%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        dfs(0, rooms, visited);
+
+        for (boolean room : visited) {
+            if (!room) return false;
+        }
+        return true;
+    }
+
+    private void dfs(int room, List<List<Integer>> rooms, boolean[] visited) {
+        visited[room] = true;
+
+        for (int key : rooms.get(room)) {
+            if (!visited[key]) {
+                dfs(key, rooms, visited);
+            }
+        }
+    }
+}
