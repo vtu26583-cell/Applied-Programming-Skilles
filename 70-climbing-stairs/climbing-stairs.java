616b0c92de60bf2ea426f83f7095e856db457e28
UTUKURU SAI SANATH <vtu26583@veltech.edu.in>
2026-04-16 09:45:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (40.42%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+        
+        int first = 1;  // Ways to reach step 1
+        int second = 2; // Ways to reach step 2
+        
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        
+        return second;
+    }
+}
