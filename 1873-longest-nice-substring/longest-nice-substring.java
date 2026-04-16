03debb9cd02f81392b7da642a1ed62b621a8af9e
UTUKURU SAI SANATH <vtu26583@veltech.edu.in>
2026-04-16 09:40:04 +0530

    Time: 3 ms (62.30%) | Memory: 44 MB (62.92%) - LeetSync

---
+++ 1873-longest-nice-substring/longest-nice-substring.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String longestNiceSubstring(String s) {
+        if (s.length() < 2) return "";
+        
+        Set<Character> set = new HashSet<>();
+        for (char c : s.toCharArray()) set.add(c);
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (set.contains(Character.toUpperCase(c)) && set.contains(Character.toLowerCase(c))) {
+                continue;
+            }
+            
+            String left = longestNiceSubstring(s.substring(0, i));
+            String right = longestNiceSubstring(s.substring(i + 1));
+            
+            return left.length() >= right.length() ? left : right;
+        }
+        
+        return s;
+    }
+}
